5cd1569e4dcf813d215f41a152c487798e65021c
Vindusviskermotor <andreas@williams.no>
2018-10-05 15:25:49 +0200

    Skeleton done, connection established

--- index.php
+++ index.php
@@ -16,6 +16,20 @@
 
 <body>
     
+    <?php
+
+    require('../connect.php');
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    echo "Connected successfully";
+    
+    ?>
     
 </body>
 
