d4f7ae350560e8d5c30d19ae64da6eb8fc0169ba
Vindusviskermotor <andreas@williams.no>
2018-10-06 13:55:36 +0200

    basic login form

--- index.php
+++ index.php
@@ -20,15 +20,87 @@
 
     require('../connect.php');
 
-    // Create connection
+    // Create connection to MySQL database
     $conn = mysqli_connect($servername, $username, $password, $database);
 
-    // Check connection
+    // Throw error if connection failed
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
-    echo "Connected successfully";
     
+    // Various checks for username and passwords
+    
+    $usernameError = $passwordError = "";
+    $username = $password = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["username"])) {
+        $nameError = "Name is required";
+        }
+        
+        else {
+        $username = securityCheck($_POST["username"]);
+        
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/", $username)) {
+            $usernameError = "Only letters and white space allowed"; 
+        }
+        }
+  
+        if (empty($_POST["password"])) {
+            $passwordError = "Password is required";
+        } else {
+            $password = securityCheck($_POST["password"]);
+        }
+        
+    }
+  
+    function securityCheck($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
+    ?>
+    
+    <!-- Input form for logging in --> 
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);
+        // The htmlspecialchars prevents hacker exploits by converting special characters to HTML entities. Server is a super global variable that returns the filename of the currently executing script by passing PHP_SELF.
+        ?>" method="post">
+        
+        Username: <input type="text" name="name" value="<?php echo $username; ?>">
+        
+        <?php 
+        if empty($nameError == false) {
+            echo $nameError;
+            $nameError = "";
+        }
+        echo $nameErr; 
+        ?>
+        
+        <br><br>
+        
+        Password: <input type="text" name="password" value="<?php echo $password;?>">
+        
+        <?php 
+        if empty($passwordError == false) {
+            echo $passwordError;
+            $passwordError = "";
+        }
+        echo $passwordError;
+        ?>
+        
+        <br><br>
+        <input type="submit" name="submit" value="Submit"> 
+    </form>
+    
+    <?php
+        echo "<h2>Your Input:</h2>";
+        echo $username;
+        echo "<br>";
+        echo $password;
+        echo "<br>";
     ?>
     
 </body>
