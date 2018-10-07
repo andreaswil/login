d31865e4a8fc957c6750b6f7274403a42eb9624f
Vindusviskermotor <andreas@williams.no>
2018-10-07 22:53:49 +0200

    wtf

--- login.php
+++ login.php
@@ -3,8 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_id("login-session");
-
 session_start();
 
 require('../database_login_info.php');
