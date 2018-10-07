d99fcb384f3bad31c44e2d72f65738f86e65fa7c
Vindusviskermotor <andreas@williams.no>
2018-10-08 00:52:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+
+if(isset($_SESSION['loginUsername'])){
+   header("location: profile.php");
+  } 
+
 // Chooses whic page to go to, based on button press
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
