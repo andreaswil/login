eac999c0bfec300c8d7ad68fbff40ba5aab42347
Vindusviskermotor <andreas@williams.no>
2018-10-09 21:18:31 +0200

    almost done styling

--- profile.php
+++ profile.php
@@ -20,13 +20,57 @@ if (isset($_POST["logout"])) {
 
 <!DOCTYPE HTML>  
 <html>
-<body>  
-<h2>Welcome to your profile <?php echo $_SESSION['loginUsername']?></h2>
+<head>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-    <button name="logout">Log out</button>
-    <button type="submit" name="profileBackButton">Back to homepage</button>
-</form>
+    <!-- Links for the different CSS files -->
+    <link rel="stylesheet" type="text/css" href="css/index.css?version=52">
+    <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+ 
+
+    <!-- Meta info -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Williams User System">
+    <meta name="keywords" content="HTML,CSS,JavaScript, PHP">
+    <meta name="author" content="Andreas Williams">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Williams User System</title>
+</head>
+    
+<body>  
+
+
+
+    
+<!-- The $_SERVER["PHP_SELF"] is a super global variable that returns the filename of the currently executing script. The htmlspecialchars() function converts special characters to HTML entities. This means that it will replace HTML characters like < and > with &lt; and &gt;. This prevents attackers from exploiting the code by injecting HTML or Javascript code (Cross-site Scripting attacks) in forms. -->
+
+<div id="index-card-box">
+    
+    <div id="login-card">
+    
+        <div id="login-header">Profile</div>
+        
+        <div id="greeting">Welcome,<?php echo $loginUsername?></div>
+
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+        <div id="register-button-row">
+            <button id="log-out-button" name="logout" class="button">LOG OUT</button>
+            <button id ="home-button" type="submit" name="profileBackButton" class="button">HOME</button>
+        </div>
+        </form>
+    
+    
+    
+    
+    
+
+        
+    </div>
+</div>
     
 </body>
-</html> 
+</html>
+
+
+
